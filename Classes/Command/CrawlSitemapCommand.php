0f8e5875d6b1a3c8409fa01baecfad33a83a96be
André Schließer <andy.schliesser@gmail.com>
2020-02-25 23:47:55 +0100

    Add argument to set custom request headers

--- Classes/Command/CrawlSitemapCommand.php
+++ Classes/Command/CrawlSitemapCommand.php
@@ -2,7 +2,7 @@
 
 namespace Schliesser\Sitecrawler\Command;
 
-use TYPO3\CMS\Core\Core\Environment;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -28,12 +28,16 @@ class CrawlSitemapCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Fetches a website (including all subpages), so the TYPO3 cache gets filled.')
+            ->setDescription('Fetches a website (including all sub pages), so the TYPO3 cache gets filled.')
             ->addArgument(
                 'url',
                 InputArgument::REQUIRED,
                 'The sitemap url.'
-            );
+            )
+            ->addArgument(
+                'headers',
+                InputArgument::OPTIONAL,
+                'Request header arguments in json format. Example: \'{"X-Pjax": true, "Cache-Control": "no-cache"}\'');
     }
 
     /**
@@ -42,18 +46,11 @@ class CrawlSitemapCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        $headers = null;
-        $fetchDirectory = Environment::getVarPath() . '/sitecrawler';
-
-        // Flush temp directory
-        if (!GeneralUtility::flushDirectory($fetchDirectory)) {
-            // Create temp directory
-            GeneralUtility::mkdir($fetchDirectory);
-        }
+        $requestHeaders = null;
         $url = (string)$input->getArgument('url');
-
         $output->writeln('Sitemap url: ' . $url, OutputInterface::VERBOSITY_VERBOSE);
 
+        // Validate input url
         if (!GeneralUtility::isValidUrl($url)) {
             $output->writeln('Error: Invalid url');
             return;
@@ -66,38 +63,50 @@ class CrawlSitemapCommand extends Command
             return;
         }
 
-        // Show urls in debug mode
-        $output->writeln(var_export($this->urls, true), OutputInterface::VERBOSITY_DEBUG);
-
+        // Return on empty urls
+        if (!$this->urls) {
+            $output->writeln('No urls found');
+            return;
+        }
         $output->writeln('Found ' . count($this->urls) . ' urls to crawl');
+        // Show urls in debug mode
+        $output->writeln('Urls: ' . var_export($this->urls, true), OutputInterface::VERBOSITY_DEBUG);
+
+        // Set headers from argument
+        if ($input->getArgument('headers')) {
+            $requestHeaders = json_decode($input->getArgument('headers'), true);
+            $output->writeln('Headers: ' . var_export($requestHeaders, true), OutputInterface::VERBOSITY_DEBUG);
+        }
+
+        // Init progress bar
+        $progressBar = new ProgressBar($output, count($this->urls));
 
         //check if sitemap has sub sitemaps
         foreach ($this->urls as $url) {
-            $output->writeln($url, OutputInterface::VERBOSITY_VERBOSE);
-            // todo: call url with or without pjax header
-            $output->writeln(GeneralUtility::getUrl($url, 2, $headers));
+            GeneralUtility::getUrl($url, 2, $requestHeaders, $error);
+            if ($error) {
+                $this->errors[] = $error;
+            }
+            $progressBar->advance();
         }
 
+        // Stop progress bar
+        $progressBar->finish();
+
+        // Print errors or success
         if ($this->errors) {
+            $output->writeln(' Finished with some errors!');
             $this->printErrors($output);
+        } else {
+            $output->writeln(' Completed successfully!');
         }
-
-        /**
-         * curl "https://www.doeser-gruppe.de/?sitemap=pages&type=1533906435&cHash=200804aefda721ee8f2b0302caffd3fa"
-         * | grep -e loc
-         * | sed 's|<loc>\(.*\)<\/loc>$|\1|g'
-         * | xargs -I {} curl -s -o /dev/null -w "%{http_code} %{url_effective}\n" -H "x-pjax: true" {}
-         */
-
-        // Crawl sitemap website
-        // exec('curl -q -r ' . $input->getArgument('sitemapUrl') . ' -P ' . $fetchDirectory);
     }
 
     protected function printErrors(OutputInterface $output)
     {
         // print errors
         foreach ($this->errors as $error) {
-            $output->writeln($error->getCode() . ': ' . $error->getMessage());
+            $output->writeln($error['error'] . ': ' . $error['message']);
         }
 
         // reset errors
@@ -112,7 +121,7 @@ class CrawlSitemapCommand extends Command
         try {
             $xml = simplexml_load_string(GeneralUtility::getUrl($siteMapUrl));
         } catch (Exception $e) {
-            $this->errors[] = $e;
+            $this->errors[] = ['error' => $e->getCode(), 'message' => $e->getMessage()];
             return;
         }
         $siteMap = json_decode(json_encode($xml), true) ?: [];
@@ -122,7 +131,6 @@ class CrawlSitemapCommand extends Command
                     $params = GeneralUtility::explodeUrl2Array(parse_url($url)['query']);
                     if (array_key_exists('sitemap', $params) || (int)$params['type'] === 1533906435) {
                         $this->getUrlListFromSiteMap($url);
-                        $this->urls[] = $url;
                     } else {
                         $this->urls[] = $url;
                     }
